8e447426cd32192ae569b3d106d7b2e11567aa30
Oti254 <otrated555@gmail.com>
2025-08-04 00:22:04 +0300

    checker compliance

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
     except ValueError:
         return ("Error: Please enter numeric values only.")
     except ZeroDivisionError:
-        return ("Error: Cannot divide by zero")
+        return ("Error: Cannot divide by zero.")
     else:
         return (f"The result of the division is {result}")
 
